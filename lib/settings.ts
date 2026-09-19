30cdc46dfa3c3ace9e89eab9030b061b2c0e596c
rjfelizardo <rjfelizardo@gmail.com>
2026-09-19 08:20:51 -0300

    Create settings.ts

---
+++ lib/settings.ts
@@ -0,0 +1,31 @@
+export interface BusinessSettings {
+  company_id: string;
+  base_price_m2: number;
+  minimum_quote: number;
+  travel_cost: number;
+  water_cost: number;
+  power_cost: number;
+  whatsapp_number: string;
+  company_name: string;
+  company_email: string;
+  company_phone: string;
+  company_address: string;
+  primary_color: string;
+  updated_at: string;
+}
+
+export const DEFAULT_SETTINGS: BusinessSettings = {
+  company_id: "default",
+  base_price_m2: 8.0,
+  minimum_quote: 2500,
+  travel_cost: 400,
+  water_cost: 250,
+  power_cost: 300,
+  whatsapp_number: "",
+  company_name: "Drone Quote",
+  company_email: "",
+  company_phone: "",
+  company_address: "",
+  primary_color: "#1B6FC9",
+  updated_at: "",
+};
